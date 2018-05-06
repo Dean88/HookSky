79efbf2425bbabdb1735ee36137fcc736ca2a2b7
Dean__88 <837899032@qq.com>
2018-05-06 17:10:13 +0800

    *:add HookSky base code

---
+++ HookSky/Source/HookSky/HookSky.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "HookSky.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HookSky, "HookSky" );
+ 
---
+++ HookSky/Source/HookSky/HookSkyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HookSkyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHookSkyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHookSkyGameMode();
+};
+
+
+
